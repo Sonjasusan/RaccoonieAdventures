c9179ef81b61aa1ba20a13aad662ea9209c3565c
Sonjasusan <sonja.nyman92@gmail.com>
2022-09-13 14:17:41 +0300

    Player & CameraController
    Pelaaja liikkumaan ja kamera seuraamaan pelaajaa

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))] //Varmistetaan ett‰ NavMeshAgent on aina
 
 public class PlayerController : MonoBehaviour
 {
-    public LayerMask movementMask;
+    //Liikkumista varten (+ NavMeshAgent)
+    NavMeshAgent agent;
 
+
+    //Raycastia varten
+    public LayerMask movementMask;
     Camera cam;
 
     // Start is called before the first frame update
     void Start()
     {
-        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>(); //Haetaan NavMeshAgent -komponentti
+        cam = Camera.main; //Kameran m‰‰ritys
         
     }
 
@@ -31,11 +39,31 @@ public class PlayerController : MonoBehaviour
                 Debug.Log("Osuttiin:" + hit.collider.name + " "+ hit.point);
 
                 //Liikutetaan pelaaja mihin klikattiin
-
+                MoveToPoint(hit.point);
                 //Pelaaja pois objectista -> esim klikkasi sient‰, muttei keskity siihen
 
             }
         }
-        
+
+        //Hiiren kakkos klikki
+        if (Input.GetMouseButton(1)) //1 = left-klikki
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            //Jos klikki osuu johonkin (esim puuhun)
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                //Katsotaan osuttiinko interactableen - jos osuttiin keskityt‰‰n (set focus) siihen
+
+            }
+        }
+
+    }
+
+    //Liikkuminen NavMeshAgentilla
+    public void MoveToPoint(Vector3 point)
+    {
+        agent.SetDestination(point);
     }
 }
