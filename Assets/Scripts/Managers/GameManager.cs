bb2526e8f02e083ef1c92092c58c5147261d9a81
Sonjasusan <sonja.nyman92@gmail.com>
2022-10-07 15:18:35 +0300

    Collectables
    Kerättäviä (timantti, tähti, sydän) lisätty kentälle ja ItemCollector -skripti luotu keräilemistä varten

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -2,8 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameManager : Singleton<GameManager>
 {
-    public GameObject Player;
-}
+    public static GameManager gameManager { get; private set; } //staattinen gamemanageri
+    public GameObject Player; //player
+
+    public static int diamonds = 0; //timantit
+
+
+    void Awake()
+    {
+            if (gameManager != null && gameManager != this) //Jos tehdään uusi gamemanager
+            {
+                Destroy(this); //tuhotaan tämä
+            }
+            else
+            {
+                gameManager = this; //Muussa tapauksessa tää on SE gamemanager
+            }
+
+            
+            if (diamonds > 1) //jos timantteja on enemmän kuin 1
+            {
+                DontDestroyOnLoad(this.gameObject); //ei tuhota ladatessa
+            }
+        }
+    }
+
