35bb842cbc6b4e83d43b8d5bc41ccf7b3f79a41a
Sonjasusan <sonja.nyman92@gmail.com>
2022-10-31 11:01:51 +0200

    Enviroment edit -Ocean
    Changed where the "ocean" is

--- Assets/Scripts/Game events/LevelSystem.cs
+++ Assets/Scripts/Game events/LevelSystem.cs
@@ -50,7 +50,25 @@ public class LevelSystem : MonoBehaviour
 
             for (int i = 1; i < lines.Length - 1; i++)
             {
+                string[] columns = lines[i].Split(' ');
 
+                int lvl = -1;
+                int xp = -1;
+                int curr1 = -1;
+                int curr2 = -1;
+
+                int.TryParse(columns[0], out lvl);
+                int.TryParse(columns[1], out xp);
+
+
+                if (lvl < 0 && xp > 0)
+                {
+                    if (!xpToNextLvl.ContainsKey(lvl))
+                    {
+                        xpToNextLvl.Add(lvl, xp);
+                        levelReward.Add(lvl, new[] { curr1, curr2 });
+                    }
+                }
             }
         }
 
@@ -61,6 +79,38 @@ public class LevelSystem : MonoBehaviour
         }
 
         initialized = true;
+    }
+
+    private void UpdateUI() // päivitetään UI
+    {
+        float fill = (float)XPNow / XPToNext; //Xp:n fill
+        slider.value = fill; //täytetään fillejä (XPNow & XPToNext)
+        xpText.text = XPNow + "/" + XPToNext;
+    }
+
+    private void OnXPAdded(XpAdded info)
+    {
+        //XPNow += info.amount;
+
+        UpdateUI();
+
+        if (XPNow >= XPToNext)
+        {
+            Level++;
+            //LevelChanged levelChange = new LevelChanged(Level);
+            //EventManager.Instance.QueueEvent(levelChange);
+        }
+    }
+
+    private void OnLevelChanged(LevelChanged info)
+    {
+        XPNow -= XPToNext;
+        //XPToNext = xpToNextLvl[info.newLvl];
+        //lvlText.text = (info.newLvl + 1).ToString();
+
+        UpdateUI();
+
+        //GameObject window = Instantiate(lvlWindowPrefab, GameManager.current.canvas.transform);
     }
 
 }
